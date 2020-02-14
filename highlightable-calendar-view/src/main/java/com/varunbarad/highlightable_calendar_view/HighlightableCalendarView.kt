3b1839b716ec6d6ece5388b98e26983f03e40609
Varun Barad <varun@varunbarad.com>
2020-02-14 14:11:03 +0530

    Declare view with its styleable attributes

---
+++ highlightable-calendar-view/src/main/java/com/varunbarad/highlightable_calendar_view/HighlightableCalendarView.kt
@@ -0,0 +1,12 @@
+package com.varunbarad.highlightable_calendar_view
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.FrameLayout
+
+class HighlightableCalendarView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : FrameLayout(context, attrs, defStyleAttr) {
+}
