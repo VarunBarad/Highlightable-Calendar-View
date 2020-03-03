f575b19bc707e16aab4cea9fcb500a066fcad5db
Varun Barad <varun@varunbarad.com>
2020-03-03 15:39:30 +0530

    Add a class which will later represent highlighting that need to be applied in calendar

---
+++ highlightable-calendar-view/src/main/java/com/varunbarad/highlightable_calendar_view/DayProperties.kt
@@ -0,0 +1,38 @@
+package com.varunbarad.highlightable_calendar_view
+
+import androidx.annotation.ColorInt
+import com.varunbarad.highlightable_calendar_view.util.isSameDay
+import java.util.*
+
+class DayProperties(
+    date: Calendar,
+    @ColorInt val textColor: Int,
+    @ColorInt val backgroundColor: Int
+) {
+    val date: Calendar = (date.clone() as Calendar).apply {
+        set(Calendar.HOUR_OF_DAY, 0)
+        set(Calendar.MINUTE, 0)
+        set(Calendar.SECOND, 0)
+        set(Calendar.MILLISECOND, 0)
+    }
+
+    fun clone(): DayProperties {
+        return DayProperties(
+            date = (this.date.clone() as Calendar),
+            textColor = this.textColor,
+            backgroundColor = this.backgroundColor
+        )
+    }
+
+    override fun equals(other: Any?): Boolean {
+        return if ((other != null) && (other is DayProperties)) {
+            this.date.isSameDay(other.date) && (this.textColor == other.textColor) && (this.backgroundColor == other.backgroundColor)
+        } else {
+            false
+        }
+    }
+
+    override fun hashCode(): Int {
+        return (this.date.timeInMillis + this.textColor + this.backgroundColor).toInt()
+    }
+}
